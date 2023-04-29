d7cce97eafcf7ea69c946c709271103d703a4009
FacuVCanale <facundovcanale@gmail.com>
2023-04-29 11:05:34 -0300

    Punto 3

---
+++ Punto_3.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+from P2 import pedir_nombre_archivo
+
+def arc_to_str(arc:str) -> str:
+    texto = ""
+    with open(arc) as f:
+        l = f.readlines()
+    for i in l:
+        texto += i
+    return texto
+
+def Fi(letra:str, texto:str) -> int:
+    cantidad = 0
+    for i in texto:
+        if i == letra:
+            cantidad += 1
+    return cantidad
+
+def IoC(letras:list, texto:str) -> int:
+    valores = []
+    for letra in letras:
+        valores.append(Fi(letra, texto))
+    promedio = sum(valores) / len(letras)
+    return promedio
+
+def Friedman_graph(texto:str):
+    # probamos con largos de clave de 1 a 30:
+    for i in range(1, 30):
+    # división del texto según el largo de la contra (i)
+        pass
+    if i == 0.0686:
+        return i             # ESTO ES CUALQUIER COSA PARA ENTENDERME
+
+def freq_analysis(ing_dict:dict, i:int):
+    # Según el largo de la clave, recorremos el texto con indexado, y sacamos el IoC de cada letra, de cada array según el largo, y hacemos el gráfico.
+    pass
+
+# leer el archivo
+
+
+#LLAMAMOS FRIEDMAN GRAPH
+# para cada largo, hacemos la formula del índice de coincidencia, y después lo promediamos (?
+# ese valor que nos da por cada largo de la clave, lo almacenamos (lista?) ya que tenemos que imprimir un gráfico que muestre cuánto da ese valor para cada largo
+# imprimimos el gráfico, con valores del 0 al 30 en x, y con valores del 0% al 7% en y. además, una recta y = 3,85% y otra y = 6,86%.
+
+#   TENGO QUE DIVIDIR EL TEXTO SEGÚN LA CLAVE QUE ME QUEDÓ, Y ASI PUEDO ANALIZAR LA FRECUENCIA DE CADA LETRA SEGÚN ESAS POSICIONES. ESO ESO ESO, POSICIONES, NO DIVIDIR EL TEXTO GOOOD.
+
+# mostrar los dos graficos.
+def main():
+    arc = pedir_nombre_archivo()
